2f7649032359d3e207f8d964f551bd2f9fc8c192
Phil Johnston <support@mintplugins.com>
2014-10-06 14:58:17 -0400

    Leading Becomes Line Height

--- includes/misc-functions/content-filters-css.php
+++ includes/misc-functions/content-filters-css.php
@@ -46,19 +46,27 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 	$downloadgrid_titles_placement = mp_core_get_post_meta($post_id, 'downloadgrid_titles_placement', 'below_image_left');
 	
 	//Title Color and size
-	$downloadgrid_title_color = mp_core_get_post_meta($post_id, 'downloadgrid_title_color', 'inherit');
+	$downloadgrid_title_color = mp_core_get_post_meta($post_id, 'downloadgrid_title_color', 'NULL');
 	$downloadgrid_title_size = mp_core_get_post_meta($post_id, 'downloadgrid_title_size', '20');
-	$downloadgrid_title_leading = mp_core_get_post_meta($post_id, 'downloadgrid_title_leading', '5');
+	$downloadgrid_title_lineheight = mp_core_get_post_meta($post_id, 'downloadgrid_title_lineheight', '20');
 	
 	//Show Post Title Backgrounds?
 	$downloadgrid_show_title_backgrounds = mp_core_get_post_meta($post_id, 'downloadgrid_show_title_backgrounds');
 	
 	//If we should show the title backgrounds
 	if ( $downloadgrid_show_title_backgrounds ){
+		
 		//Title background spacing (padding)
-		$downloadgrid_title_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_title_background_padding', '0');	
+		$downloadgrid_title_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_title_background_padding', '5');	
+		
+			//Calculate Minimum Line Height with Padding
+			$min_line_height_with_padding = ( $downloadgrid_title_background_padding * 3 ) + $downloadgrid_title_size;
+			//If the line height with padding is greater than the lineheight, we need to make the lineheight match or the layout gets thrown off
+			$downloadgrid_title_lineheight = $min_line_height_with_padding  > $downloadgrid_title_lineheight ? $min_line_height_with_padding : $downloadgrid_title_lineheight;
+		
 		//Title background color 
 		$downloadgrid_title_background_color = mp_core_get_post_meta($post_id, 'downloadgrid_title_background_color', '#fff' );	
+		
 		//Title background opacity 
 		$downloadgrid_title_background_opacity = mp_core_get_post_meta($post_id, 'downloadgrid_title_background_opacity', '100');	
 	}
@@ -75,17 +83,24 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 	$downloadgrid_excerpt_placement = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_placement', 'below_image_left');
 	
 	//Excerpt Color and Size
-	$downloadgrid_excerpt_color = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_color', 'inherit');
+	$downloadgrid_excerpt_color = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_color', 'NULL');
 	$downloadgrid_excerpt_size = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_size', '15');
-	$downloadgrid_excerpt_leading = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_leading', '3');
+	$downloadgrid_excerpt_lineheight = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_lineheight', '18');
 	
 	//Show Excerpt Backgrounds?
 	$downloadgrid_show_excerpt_backgrounds = mp_core_get_post_meta($post_id, 'downloadgrid_show_excerpt_backgrounds');
 	
 	//If we should show the excerpt backgrounds
 	if ( $downloadgrid_show_excerpt_backgrounds ){
+		
 		//Excerpt background spacing (padding)
-		$downloadgrid_excerpt_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_background_padding', '0');	
+		$downloadgrid_excerpt_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_background_padding', '5');	
+		
+			//Calculate Minimum Line Height with Padding
+			$min_line_height_with_padding = ( $downloadgrid_excerpt_background_padding * 3 ) + $downloadgrid_excerpt_size;
+			//If the line height with padding is greater than the lineheight, we need to make the lineheight match or the layout gets thrown off
+			$downloadgrid_excerpt_lineheight = $min_line_height_with_padding  > $downloadgrid_excerpt_lineheight ? $min_line_height_with_padding : $downloadgrid_excerpt_lineheight;
+		
 		//Excerpt background color 
 		$downloadgrid_excerpt_background_color = mp_core_get_post_meta($post_id, 'downloadgrid_excerpt_background_color', '#fff' );	
 		//Excerpt background opacity 
@@ -104,9 +119,8 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 	$downloadgrid_price_placement = mp_core_get_post_meta($post_id, 'downloadgrid_price_placement', 'below_image_left');
 	
 	//Price Color and Size
-	$downloadgrid_price_color = mp_core_get_post_meta($post_id, 'downloadgrid_price_color', 'inherit');
+	$downloadgrid_price_color = mp_core_get_post_meta($post_id, 'downloadgrid_price_color', 'NULL');
 	$downloadgrid_price_size = mp_core_get_post_meta($post_id, 'downloadgrid_price_size', '15');
-	$downloadgrid_price_leading = mp_core_get_post_meta($post_id, 'downloadgrid_price_leading', '3');
 	
 	//Show Price Backgrounds?
 	$downloadgrid_show_price_backgrounds = mp_core_get_post_meta($post_id, 'downloadgrid_show_price_backgrounds');
@@ -114,7 +128,7 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 	//If we should show the price backgrounds
 	if ( $downloadgrid_show_price_backgrounds ){
 		//Price background spacing (padding)
-		$downloadgrid_price_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_price_background_padding', '1');	
+		$downloadgrid_price_background_padding = mp_core_get_post_meta($post_id, 'downloadgrid_price_background_padding', '5');	
 		//Price background color 
 		$downloadgrid_price_background_color = mp_core_get_post_meta($post_id, 'downloadgrid_price_background_color', '#FFFFFF' );	
 		//Price background opacity 
@@ -130,28 +144,30 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 	}
 	
 	//Load More Buttons Colors
-	$downloadgrid_load_more_button_color = mp_core_get_post_meta($post_id, 'downloadgrid_load_more_button_color', 'inherit');
-	$downloadgrid_load_more_button_text_color = mp_core_get_post_meta($post_id, 'downloadgrid_load_more_button_text_color', 'inherit');
-	$downloadgrid_mouse_over_load_more_button_color = mp_core_get_post_meta($post_id, 'downloadgrid_mouse_over_load_more_button_color', 'inherit');
-	$downloadgrid_mouse_over_load_more_button_text_color = mp_core_get_post_meta($post_id, 'downloadgrid_mouse_over_load_more_button_text_color', 'inherit');
+	$downloadgrid_load_more_button_color = mp_core_get_post_meta($post_id, 'downloadgrid_load_more_button_color', 'NULL');
+	$downloadgrid_load_more_button_text_color = mp_core_get_post_meta($post_id, 'downloadgrid_load_more_button_text_color', 'NULL');
+	$downloadgrid_mouse_over_load_more_button_color = mp_core_get_post_meta($post_id, 'downloadgrid_mouse_over_load_more_button_color', 'NULL');
+	$downloadgrid_mouse_over_load_more_button_text_color = mp_core_get_post_meta($post_id, 'downloadgrid_mouse_over_load_more_button_text_color', 'NULL');
 	
 	//Get CSS Output
 	$css_output .= '
-		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item{ 
-			color:' . $downloadgrid_excerpt_color . ';
-			width:' . (100/$downloadgrid_per_row) .'%;
-			padding: ' . $downloadgrid_post_spacing . 'px;
-		}
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid{ ' . 
+			mp_core_css_line( 'margin', $downloadgrid_post_spacing, 'px' ) . 
+		'}
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item{' . 
+			mp_core_css_line( 'color', $downloadgrid_excerpt_color ) . 
+			mp_core_css_line( 'width', (100/$downloadgrid_per_row), '%' ) . 
+			mp_core_css_line( 'padding', $downloadgrid_post_spacing, 'px' ) . 
+		'}
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-title-holder{
 			' . mp_stacks_grid_get_text_placement_css( $downloadgrid_titles_placement, array( 
-					'line_height' => ( $downloadgrid_title_leading ),
-				) ) . ';
-	
-			color:' . $downloadgrid_title_color . ';
-			font-size:' . $downloadgrid_title_size . 'px;
-			line-height:' . ( $downloadgrid_title_size + $downloadgrid_title_leading ) . 'px;
-		}
-		' . mp_stacks_grid_highlight_text_css( array( 
+					'line_height' => ( $downloadgrid_title_size ),
+				) ) . ';' . 
+			mp_core_css_line( 'color', $downloadgrid_title_color ) . 
+			mp_core_css_line( 'font-size', $downloadgrid_title_size, 'px' ) . 
+			mp_core_css_line( 'line-height', $downloadgrid_title_lineheight, 'px' ) . 
+		'}' . 
+		mp_stacks_grid_highlight_text_css( array( 
 				'brick_id' => $post_id,
 				'class_name' => 'mp-stacks-downloadgrid-item-title',
 				'highlight_padding' => $downloadgrid_title_background_padding, 
@@ -161,50 +177,50 @@ function mp_stacks_brick_content_output_css_downloadgrid( $css_output, $post_id,
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-excerpt-holder, 
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-excerpt-holder a{
 			' . mp_stacks_grid_get_text_placement_css( $downloadgrid_excerpt_placement, array( 
-					'line_height' => ($downloadgrid_excerpt_leading),
-				) ) . ';
-			
-			color:' . $downloadgrid_excerpt_color . ';
-			font-size:' . $downloadgrid_excerpt_size . 'px;
-			line-height:' . ($downloadgrid_excerpt_size + $downloadgrid_excerpt_leading) . 'px;
-		}
-		' . mp_stacks_grid_highlight_text_css( array( 
+					'line_height' => ($downloadgrid_excerpt_size),
+				) ) . '; ' .
+			mp_core_css_line( 'color', $downloadgrid_excerpt_color ) . 
+			mp_core_css_line( 'font-size', $downloadgrid_excerpt_size, 'px' ) . 
+			mp_core_css_line( 'line-height', $downloadgrid_excerpt_lineheight, 'px' ) . 
+		'}' . 
+		mp_stacks_grid_highlight_text_css( array( 
 				'brick_id' => $post_id,
 				'class_name' => 'mp-stacks-downloadgrid-item-excerpt',
 				'highlight_padding' => $downloadgrid_excerpt_background_padding, 
 				'highlight_color' => $downloadgrid_excerpt_background_color, 
 				'highlight_opacity' => $downloadgrid_excerpt_background_opacity
 		) ) . '
-		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-price-holder{
+		
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-price-holder, 
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-item-price-holder a{
 			' . mp_stacks_grid_get_text_placement_css( $downloadgrid_price_placement, array( 
-					'line_height' => ($downloadgrid_price_leading),
-				) ) . ';
-			
-			color:' . $downloadgrid_price_color . ';
-			font-size:' . $downloadgrid_price_size . 'px;
-			line-height:' . ($downloadgrid_price_size + $downloadgrid_price_leading) . 'px;
-		}
-		' . mp_stacks_grid_highlight_text_css( array( 
+					'line_height' => ($downloadgrid_price_size),
+				) ) . '; ' .
+			mp_core_css_line( 'color', $downloadgrid_price_color ) . 
+			mp_core_css_line( 'font-size', $downloadgrid_price_size, 'px' ) . 
+		'}' . 
+		mp_stacks_grid_highlight_text_css( array( 
 				'brick_id' => $post_id,
 				'class_name' => 'mp-stacks-downloadgrid-item-price',
 				'highlight_padding' => $downloadgrid_price_background_padding, 
 				'highlight_color' => $downloadgrid_price_background_color, 
 				'highlight_opacity' => $downloadgrid_price_background_opacity
 		) ) . '
-		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-load-more-button{
-			color:' . $downloadgrid_load_more_button_text_color  . ';
-			background-color:' . $downloadgrid_load_more_button_color  . ';
-		}
-		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-load-more-button:hover{
-			color:' . $downloadgrid_mouse_over_load_more_button_text_color  . ';
-			background-color:' . $downloadgrid_mouse_over_load_more_button_color  . ';
-		}
+		
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-load-more-button{' . 
+			mp_core_css_line( 'color', $downloadgrid_load_more_button_text_color ) . 
+			mp_core_css_line( 'background-color', $downloadgrid_load_more_button_color ) . 
+		'}
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-load-more-button:hover{' . 
+			mp_core_css_line( 'color', $downloadgrid_mouse_over_load_more_button_text_color ) . 
+			mp_core_css_line( 'background-color', $downloadgrid_mouse_over_load_more_button_color ) . 
+		'}
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-over-image-text-container,
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-over-image-text-container-top,
 		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-over-image-text-container-middle,
-		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-over-image-text-container-bottom{
-			padding: ' . $downloadgrid_featured_images_inner_margin . 'px;
-		}';
+		#mp-brick-' . $post_id . ' .mp-stacks-downloadgrid-over-image-text-container-bottom{' . 
+			mp_core_css_line( 'padding', $downloadgrid_featured_images_inner_margin, 'px' ) . 
+		'}';
 		
 		return $css_output;
 	
