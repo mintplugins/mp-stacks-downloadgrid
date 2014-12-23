05f1251697bb14401a7d01aad8907d9e18d74f54
Phil Johnston <support@mintplugins.com>
2014-12-23 18:14:16 -0500

    Added Plugin Activation Function

--- mp-stacks-downloadgrid.php
+++ mp-stacks-downloadgrid.php
@@ -91,6 +91,11 @@ function mp_stacks_downloadgrid_textdomain() {
 }
 add_action( 'init', 'mp_stacks_downloadgrid_textdomain', 1 );
 
+/**
+ * Activation Hook Function - Sets up Rewrite Rules etc
+ */
+require( MP_STACKS_DOWNLOADGRID_PLUGIN_DIR . 'includes/misc-functions/install.php' );
+
 /*
 |--------------------------------------------------------------------------
 | INCLUDES
