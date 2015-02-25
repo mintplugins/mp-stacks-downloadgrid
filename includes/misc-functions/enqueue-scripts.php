0a614edce32ffe5d53053e1fd57cc2caada63c73
Phil Johnston <support@mintplugins.com>
2015-02-25 10:48:22 -0500

    Enqueue versions

--- includes/misc-functions/enqueue-scripts.php
+++ includes/misc-functions/enqueue-scripts.php
@@ -28,16 +28,16 @@
 function mp_stacks_downloadgrid_enqueue_scripts(){
 	
 	//Enqueue velocity JS
-	wp_enqueue_script( 'velocity_js', MP_CORE_JS_SCRIPTS_URL . 'velocity.min.js', array( 'jquery' ) );
+	wp_enqueue_script( 'velocity_js', MP_CORE_JS_SCRIPTS_URL . 'velocity.min.js', array( 'jquery' ), MP_STACKS_DOWNLOADGRID_VERSION );
 	
 	//Enqueue Waypoints JS
-	wp_enqueue_script( 'waypoints_js', MP_CORE_JS_SCRIPTS_URL . 'waypoints.min.js', array( 'jquery' ) );
+	wp_enqueue_script( 'waypoints_js', MP_CORE_JS_SCRIPTS_URL . 'waypoints.min.js', array( 'jquery' ), MP_STACKS_DOWNLOADGRID_VERSION );
 	
 	//masonry script
 	wp_enqueue_script( 'masonry' );
 			
 	//Enqueue downloadgrid JS
-	wp_enqueue_script( 'mp_stacks_downloadgrid_js', plugins_url( 'js/downloadgrid.js', dirname( __FILE__ ) ), array( 'jquery', 'velocity_js', 'masonry', 'waypoints_js' ) );
+	wp_enqueue_script( 'mp_stacks_downloadgrid_js', plugins_url( 'js/downloadgrid.js', dirname( __FILE__ ) ), array( 'jquery', 'velocity_js', 'masonry', 'waypoints_js' ), MP_STACKS_DOWNLOADGRID_VERSION );
 
 }
  
